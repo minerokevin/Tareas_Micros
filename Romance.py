fc88ef717d77db855dcd0102452acad3d996c216
mongedaniel <79485532+mongedaniel@users.noreply.github.com>
2021-02-22 17:44:39 -0600

    Add files via upload

---
+++ Romance.py
@@ -0,0 +1,9 @@
+def Miamorpor__ (xoxo):
+    if xoxo == "Kevin":
+        print ("Es infinito")
+    else:
+        print("No existe")
+
+persona = input("Ingrese el nombre de la perona: ")
+Miamorpor (persona)
+
